1d869a24e4f9b0f095d70b582619e29f15341a1b
Adarsh singh <adarshsingh345125@gmail.com>
2022-07-16 22:34:04 +0530

    cors solve

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const connectToMongo =require("./db")
 const express = require('express')
-var cors = require('cors')
+// var cors = require('cors')
 
 connectToMongo();
 const app = express()
 const port = process.env.PORT || 80
 
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
